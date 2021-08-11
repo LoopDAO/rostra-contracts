ba736a33403250b7fbaf90e3a84803a27b4e011f
HA <>
2021-08-11 14:45:35 +0800

    refactor test

--- test/project-test.js
+++ test/project-test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Project contract", function () {
     let projectContractFactory;
@@ -9,12 +10,10 @@ describe("Project contract", function () {
 
     beforeEach(async function () {
         [creator,donator1,donator2] = await ethers.getSigners();
-        console.log(creator.address)
         projectContractFactory = await ethers.getContractFactory("Project");
         const expiretime = parseInt(new Date().getTime()/1000) + 7 * 24 * 60 * 60
 
         projectContract = await projectContractFactory.deploy(creator.address,"Buy some toys","Buy some toys",expiretime,100);
-        await projectContract.deployed();
 
         expect(projectContract.address).to.not.be.null;
     });
@@ -35,11 +34,9 @@ describe("Project contract", function () {
             value: 10,
         }
 
-        const artifact = artifacts.readArtifactSync("Project")
-        const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-        await transactionContract.contribute(overrides)
+        await projectContract.connect(donator1).contribute(overrides)
 
-        expect(parseInt(await transactionContract.currentBalance())).to.equal(10)
+        expect(parseInt(await projectContract.currentBalance())).to.equal(10)
     })
 
 
@@ -49,25 +46,9 @@ describe("Project contract", function () {
             value: 101,
         }
 
-        const artifact = artifacts.readArtifactSync("Project")
-        const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-        await transactionContract.contribute(overrides)
+        await projectContract.connect(donator1).contribute(overrides)
 
-        expect(parseInt(await transactionContract.state())).to.equal(2)
-    })
-
-
-    it('Payout to the creator', async function () {
-        let overrides = {
-            // To convert Ether to Wei:
-            value: 101,
-            gasPrice: 1000000000
-        }
-
-        const artifact = artifacts.readArtifactSync("Project")
-        const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-        await transactionContract.contribute(overrides)
-
+        expect(parseInt(await projectContract.state())).to.equal(2)
     })
 
 });
