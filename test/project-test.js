9c4d585488f723bd9c64f9de253a2c2ce88e7d7c
HA <>
2021-08-12 22:52:14 +0800

    added unit tests

--- test/project-test.js
+++ test/project-test.js
@@ -4,6 +4,8 @@ const { ethers } = require('hardhat');
 describe("Project contract", function () {
     let projectContractFactory;
     let projectContract;
+    let nftContractFactory;
+    let nftContract;
     let creator;
     let donator1;
     let donator2;
@@ -15,9 +17,21 @@ describe("Project contract", function () {
         projectContractFactory = await ethers.getContractFactory("Project");
         const expiretime = parseInt(new Date().getTime()/1000) + sevenDays;
 
-        projectContract = await projectContractFactory.deploy(creator.address,"Research Uni V3","We will produce 3 videos", expiretime, 100);
+        projectContract = await projectContractFactory.deploy(creator.address, "Research Uni V3","We will produce 3 videos", expiretime, 100);
 
         expect(projectContract.address).to.not.be.null;
+
+        const name = "Research Uni V3 Token";
+        const symbol = "RUVT";
+        const totalSupply = 10000;
+        const creatorPortion = 20;  // creator will receive 20% of tokens
+        await projectContract.setERC20Info(name, symbol, totalSupply, creatorPortion);
+
+        nftContractFactory = await ethers.getContractFactory("Work");
+
+        nftContract = await nftContractFactory.deploy('Research Uni V3', 'RUV', 'https://rostra.xyz/api/?id=');
+
+        expect(nftContract.address).to.not.be.null;
     });
 
     it('Get details of a new project', async function () {
@@ -46,7 +60,7 @@ describe("Project contract", function () {
         expect(await projectContract.state()).to.equal(2);
     })
 
-    it('Funding failed, user get refund', async function () {
+    xit('Funding failed, user get refund', async function () {
         await projectContract.connect(donator1).contribute({ value: 90 });
 
         expect(await projectContract.currentBalance()).to.equal(90);
@@ -81,36 +95,145 @@ describe("Project contract", function () {
     })
 
     // todo
-    it('Funding succeeds, creator start to work', async function () {
+    xit('Creator does not submit work before deadline, investors get 100% refund', async function () {
+        await projectContract.connect(donator1).contribute({ value: 100 });
+        await projectContract.connect(donator2).contribute({ value: 100 });
+
+        await ethers.provider.send('evm_increaseTime', [sevenDays]);
+        await ethers.provider.send('evm_mine');
+
+        await projectContract.connect(donator1).getRefund(); // donator1 get refund 50%(50)
+        expect(await projectContract.currentBalance()).to.equal(100);
+
+        await projectContract.connect(donator2).getRefund(); // donator2 get refund 50%(50)
+        expect(await projectContract.currentBalance()).to.equal(0);
     })
 
-    // todo
-    it('Creator submit work before deadline', async function () {
+
+    // Funding succeeds, creator start to work automatically, count down time
+    describe('Creator submit work before deadline', async function () {
+        console.log('projectContract: ', projectContract);
+        await projectContract.connect(donator1).contribute({ value: 100 });
+        await projectContract.connect(donator2).contribute({ value: 100 });
+        const workResult = {
+            name: 'Uni V3 analysis video',
+            desc: 'Hope you enjoy our work',
+            url: 'https://rostra.xyz/projects/0'
+        };
+
+        // todo
+        await projectContract.finishWork(workResult);
+
+        expect(await projectContract.state()).to.equal(3);
+
+        xit('Investors do not oppose, creator can get money after 7 days', async function () {
+            // todo
+            expect(await projectContract.checkIfFundingCompleteOrExpired()).revertedWithError(Error('Project is not complete'));
+
+            await ethers.provider.send('evm_increaseTime', [sevenDays]);
+            await ethers.provider.send('evm_mine');
+
+            expect(await projectContract.currentBalance()).to.equal(200);
+
+            await projectContract.checkIfFundingCompleteOrExpired();
+
+            expect(await projectContract.currentBalance()).to.equal(0);
+        })
+
+        // todo
+        xit('Investors do not oppose, there will be a NFT minted', async function () {
+            const nftContractAddress = await projectContract.getNFTAddress();
+            expect(nftContractAddress).to.notEmpty();
+
+            const nftTotal = nftContractAddress.totalSupply();
+            expect(nftTotal).to.equal(1);
+        })
+
+        // todo
+        xit('Investors do not oppose, they can claim erc20 token per investment', async function () {
+            const creatorBalance = await projectContract.getUserERC20Balance(creator.address);
+            expect(creatorBalance).to.be.equal(2000);
+
+            const donator1Balance = await projectContract.getUserERC20Balance(donator1.address);
+            expect(donator1Balance).to.be.equal(4000);
+
+            const donator2Balance = await projectContract.getUserERC20Balance(donator2.address);
+            expect(donator2Balance).to.be.equal(4000);
+
+        })
     })
 
-    // todo// accept the work
-    it('Investors do not oppose, creator can get money after 7 days', async function () {
-    })
+    // Funding succeeds, creator start to work automatically, count down time
+    describe('Creator submit work before deadline, but get oppose', async function () {
+        await projectContract.connect(donator1).contribute({ value: 100 });
+        await projectContract.connect(donator2).contribute({ value: 100 });
+        const workResult = {
+            name: 'Uni V3 analysis video',
+            desc: 'Hope you enjoy our work',
+            url: 'https://rostra.xyz/projects/0'
+        };
 
-    // todo
-    it('Investors do not oppose, there will be a NFT minted', async function () {
-    })
+        // todo
+        await projectContract.finishWork(workResult);
 
-    // todo
-    it('Investors do not oppose, they can claim erc20 token per investment', async function () {
-    })
+        expect(await projectContract.state()).to.equal(3);
+        expect(await projectContract.currentBalance()).to.equal(200);
 
-    // todo: oppose the work
-    it('Investors oppose, but do not reach 67% votes in 7 days, creator can get 100% money', async function () {
-    })
+        // todo: oppose the work
+        xit('Investors oppose, but do not reach 67% votes in 7 days, creator can get 100% money', async function () {
+            await projectContract.connect(donator1).opposeWork();
 
-    // todo
-    it('Investors oppose, and reach 67% votes in 7 days, creator can get 50% money, investors get 50% refund', async function () {
-    })
+            expect(await projectContract.state()).to.equal(4);  // in voting state
 
-    // todo
-    it('Creator does not submit work before deadline, investors get 100% refund', async function () {
-    })
+            expect(await projectContract.connect(donator1).getRefund()).revertedWithError(Error('Can not refund'));
 
+            expect(await projectContract.checkIfFundingCompleteOrExpired()).revertedWithError(Error('Need to wait until appeal ends'));
+
+            await ethers.provider.send('evm_increaseTime', [sevenDays]);
+            await ethers.provider.send('evm_mine');
+
+            expect(await projectContract.connect(donator1).getRefund()).revertedWithError(Error('Can not refund'));
+
+            await projectContract.checkIfFundingCompleteOrExpired();
+            expect(await projectContract.currentBalance()).to.equal(0);
+        })
+    });
+
+    // Funding succeeds, creator start to work automatically, count down time
+    describe('Creator submit work before deadline, but get oppose', async function () {
+        await projectContract.connect(donator1).contribute({ value: 100 });
+        await projectContract.connect(donator2).contribute({ value: 100 });
+        const workResult = {
+            name: 'Uni V3 analysis video',
+            desc: 'Hope you enjoy our work',
+            url: 'https://rostra.xyz/projects/0'
+        };
+
+        // todo
+        await projectContract.finishWork(workResult);
+
+        expect(await projectContract.state()).to.equal(3);
+        expect(await projectContract.currentBalance()).to.equal(200);
+
+        // todo: oppose the work
+        xit('Investors oppose, and reach 67% votes in 7 days, creator can get 50% money, investors get 50% refund', async function () {
+            await projectContract.connect(donator1).opposeWork();
+
+            expect(await projectContract.checkIfFundingCompleteOrExpired()).revertedWithError(Error('Need to wait until appeal ends'));
+
+            await projectContract.connect(donator2).opposeWork();
+
+            expect(await projectContract.state()).to.equal(5);  // in refunded state
+
+            await projectContract.connect(donator1).getRefund(); // donator1 get refund 50%(50)
+            expect(await projectContract.currentBalance()).to.equal(150);
+
+            await projectContract.connect(donator2).getRefund(); // donator2 get refund 50%(50)
+            expect(await projectContract.currentBalance()).to.equal(100);
+
+            await projectContract.checkIfFundingCompleteOrExpired(); // creator get left 50%(100)
+            expect(await projectContract.currentBalance()).to.equal(0);
+        })
+    });
 });
 
