5872f3509603b5037c1a1a3f86f6d5cf2d497401
HA <>
2022-01-10 00:17:26 +0800

    erc1155 contract

---
+++ contracts/AbstractERC1155Factory.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+
+
+abstract contract AbstractERC1155Factory is
+    ERC1155PausableUpgradeable,
+    ERC1155SupplyUpgradeable,
+    ERC1155BurnableUpgradeable,
+    OwnableUpgradeable
+{
+    string public name_;
+    string public symbol_;
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function setURI(string memory baseURI) external onlyOwner {
+        _setURI(baseURI);
+    }
+
+    function name() public view returns (string memory) {
+        return name_;
+    }
+
+    function symbol() public view returns (string memory) {
+        return symbol_;
+    }
+
+    function _mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual override(ERC1155Upgradeable, ERC1155SupplyUpgradeable) {
+        super._mint(account, id, amount, data);
+    }
+
+    function _mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155Upgradeable, ERC1155SupplyUpgradeable) {
+        super._mintBatch(to, ids, amounts, data);
+    }
+
+    function _burn(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) internal virtual override(ERC1155Upgradeable, ERC1155SupplyUpgradeable) {
+        super._burn(account, id, amount);
+    }
+
+    function _burnBatch(
+        address account,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) internal virtual override(ERC1155Upgradeable, ERC1155SupplyUpgradeable) {
+        super._burnBatch(account, ids, amounts);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155PausableUpgradeable, ERC1155Upgradeable) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
