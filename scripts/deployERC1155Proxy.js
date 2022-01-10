297f618ad24133ecd29a48006750a8a9c2f21b66
HA <>
2022-01-10 18:41:36 +0800

    add deploy script

---
+++ scripts/deployERC1155Proxy.js
@@ -0,0 +1,35 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Contract = await ethers.getContractFactory("ERC1155Proxy");
+
+  const result = await upgrades.deployProxy(Contract, [
+    "ipfs://"
+  ]);
+
+  console.log("Contract address:", result.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
