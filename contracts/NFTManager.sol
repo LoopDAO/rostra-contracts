632487d21ea41adcee76ba1fad2780a7e4808ecd
HA <>
2022-01-24 23:20:12 +0800

    add nft manager

---
+++ contracts/NFTManager.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "./ERC1155Proxy.sol";
+
+contract NFTManager {
+    function mintNewNFT(address _nftAddress, string memory _uri, address[] memory _addresses)
+        external
+    {
+
+    }
+
+    function mintExistingNFT(address _nftAddress, string memory _uri, address[] memory _addresses)
+        external
+    {
+
+    }
+}
