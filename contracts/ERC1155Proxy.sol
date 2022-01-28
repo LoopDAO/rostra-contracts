a36cbe7192e6c7b17d2a566210c303ac2ddcc6a2
coder <>
2022-01-28 14:37:39 +0800

    update solidity version: ^0.8.0 solc: 0.8.4

--- contracts/ERC1155Proxy.sol
+++ contracts/ERC1155Proxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/presets/ERC1155PresetMinterPauserUpgradeable.sol";
 
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.0;
 
 // import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 // import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
