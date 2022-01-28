959c5b4b3e544d0ae8a8b373e25b4a2dd5d8f17d
HA <>
2022-01-28 21:58:12 +0800

    add deploy script

---
+++ scripts/deployNFTManager.ts
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers, upgrades } from "hardhat"
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Contract = await ethers.getContractFactory("NFTManager");
+
+  const result = await upgrades.deployProxy(Contract, []);
+
+  console.log("Contract address:", result.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
